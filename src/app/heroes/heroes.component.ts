339af2599c44f96a905f4e2e44d4803cc125f786
william <williamrochajr@gmail.com>
2024-08-07 21:38:32 -0300

    feat: display select list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
